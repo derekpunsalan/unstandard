dd062dd3f9f2ae2088e6f3d20f07392dafeb0c85
Derek Punsalan <derek@punsalan.me>
2014-10-20 07:15:27 -0700

    Set post thumbs as background-images

--- index.php
+++ index.php
@@ -1,13 +1,8 @@
-<?php
-/**
- * @package Unstandard
- * @since Unstandard 3.0
- */
-get_header(); ?>
+<?php get_header(); ?>
 
-<div class="north">
+<div class="content-box">
 
-	<div class="main-column grid-8<?php if ( is_search() ) {?> search-page<?php } elseif ( ! is_archive() && ! is_paged() ) { ?> front-page<?php } else { ?> back-page<?php } ?>">
+	<div class="main-column <?php if ( is_search() ) {?> search-page<?php } elseif ( ! is_archive() && ! is_paged() ) { ?> front-page<?php } else { ?> back-page<?php } ?>">
 
 		<?php if (is_search() ) { ?>
 
@@ -31,18 +26,13 @@ get_header(); ?>
 
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-		<div id="post-<?php the_ID(); ?>" class="<?php echo ( is_first_post($post->ID) ) ? 'primary-post grid-8 alpha omega thumb-post' : 'secondary-post grid-4 alpha omega thumb-post'; ?>">
+		<div id="post-<?php the_ID(); ?>" class="<?php echo ( is_first_post($post->ID) ) ? 'primary-post thumb-post' : 'secondary-post thumb-post'; ?>">
 
-			<a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>">
+			<?php $background_url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>
 
-				<?php if ( is_first_post($post->ID) ) { ?>
-					<?php the_post_thumbnail('primary-post-thumbnail'); ?>
-				<?php } else { ?>
-					<?php the_post_thumbnail('secondary-post-thumbnail'); ?>
-				<?php } ?>
+			<a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>" style="background-image: url('<?php echo $background_url; ?>');">
 
 				<div class="thumbnail-title">
-
 					<h2>
 						<?php if ( is_first_post($post->ID) ) { ?>
 							<?php the_title(); ?>
@@ -52,7 +42,6 @@ get_header(); ?>
 					</h2>
 
 					<span class="comment-count"><?php comments_number('0','1','%'); ?></span>
-
 				</div>
 
 			</a>
@@ -74,9 +63,6 @@ get_header(); ?>
 
 	<?php get_sidebar(); ?>
 
-	<div class="clear"></div>
-
 </div>
-
 
 <?php get_footer(); ?>
