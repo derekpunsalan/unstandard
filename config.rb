bd437a5078d849afe0ced28712eca4632e5b8c7a
Derek Punsalan <derek@punsalan.me>
2014-10-20 07:12:43 -0700

    Define config.rb

---
+++ config.rb
@@ -0,0 +1,14 @@
+http_path = "/"
+css_dir = "."
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+
+# May change this to :nested, :compact, or :compressed
+output_style = :expanded
+
+# Relative assets required for WordPress compatibility
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
