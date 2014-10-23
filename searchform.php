9a3a9ae5e843f1e352cf0a11f403b2d73cf5f6a3
Derek Punsalan <derek@punsalan.me>
2014-10-22 18:02:36 -0700

    Remove @package @since comments

--- searchform.php
+++ searchform.php
@@ -1,9 +1,3 @@
-<?php
-/**
- * @package Unstandard
- * @since Unstandard 3.0
- */
-?>
 <form class="search-form" method="get" action="<?php echo home_url(); ?>/">
 
 	<input class="search-input" type="text" name="s" id="s" value="Type and press enter"/>
--- template-fullwidth.php
+++ template-fullwidth.php
@@ -1,49 +1,45 @@
 <?php
-/**
- * @package TheUnstandard
- * @since TheUnstandard 2.0
- */
 /*
 Template Name: Full Width
 */
 get_header(); ?>
 
 <div class="north">
-		
+
 	<div class="main-column grid-12">
-		
+
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			
+
 		<div id="post-<?php the_ID(); ?>" class="single-entry group">
 
 			<h2><?php the_title(); ?></h2>
-			
+
 			<div class="post-meta group">
-				
+
 				<div class="post-date">
-					
+
 					<span class="sub-title">Posted</span>
-					
+
 					<span class="title"><?php the_time('M jS, Y') ?> <?php edit_post_link('(Edit)', '', ''); ?></span>
-					
+
 				</div>
-				
+
 				<div class="post-author">
-					
+
 					<span class="sub-title">Author</span>
-					
+
 					<span class="title"><?php the_author_posts_link(); ?></span>
-					
+
 				</div>
-				
+
 				<div class="post-comments">
-					
+
 					<span class="sub-title">Discuss</span>
-					
+
 					<span class="title"><?php comments_popup_link('No Comments', '1 Comment', '% Comments'); ?></span>
-					
+
 				</div>
-				
+
 			</div><!-- .post-meta -->
 
 			<div <?php post_class('post') ?>>
@@ -55,27 +51,27 @@ get_header(); ?>
 			</div>
 
 		</div>
-		
+
 		<div class="grid-8 alpha">
 		<?php comments_template(); ?>
 		</div>
-			
+
 		<?php endwhile; else : ?>
-			
+
 		<div class="single-entry group">
-			
+
 			<h2>Nothing Found</h2>
-			
+
 			<p>Oops! It looks like you may have stumbled upon a page that may have existed at one point - but is missing now. Please check the link and try again.</p>
-			
+
 		</div>
-			
+
 		<?php endif; ?>
-		
+
 	</div>
-		
+
 	<div class="clear"></div>
-	
+
 </div>
 
 <?php get_footer(); ?>
